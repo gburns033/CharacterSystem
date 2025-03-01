3d5fb745e894e331f2fa96d1ed26c0bc398cd2e9
Zach Goodson <quantamstudios@gmail.com>
2025-03-01 15:50:07 -0600

    Add in Damage Mechanics for Orc, Human, and Wizard

--- src/edu/gz/MiddleEarth/Orc.java
+++ src/edu/gz/MiddleEarth/Orc.java
@@ -24,8 +24,15 @@ public class Orc extends MiddleEarthCharacter {
      */
     @Override
     public boolean attack(MiddleEarthCharacter target) {
-        if (target.getRace().equals(this.getRace())) {
+        if (target != null && (target.getRace().equals(this.getRace()) || target.getRace().equals("Elf"))) {
             return false;
+        }
+        
+        if (target.getRace().equals("Human")) {
+            double truePower = power * 1.5;
+            target.health = target.health - truePower > 0 ? target.health - truePower : 0;
+
+            return true;
         }
 
         // Deal damage while ensuring health doesn't go negative
