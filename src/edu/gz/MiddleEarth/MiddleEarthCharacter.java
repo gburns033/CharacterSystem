f5ced39d316b876f9384c0a82d12e2a9309befb7
Zach Goodson <quantamstudios@gmail.com>
2025-03-01 15:23:15 -0600

    Update ME Character Constructor and Add Comments

--- src/edu/gz/MiddleEarth/MiddleEarthCharacter.java
+++ src/edu/gz/MiddleEarth/MiddleEarthCharacter.java
@@ -1,19 +1,48 @@
 package edu.gz.MiddleEarth;
 
 public abstract class MiddleEarthCharacter {
-	String name;
-	double health;
-	double power;
-	
-	public MiddleEarthCharacter () {
-		name = "";
-		health = 0;
-		power = 0;
-	}
+    protected String name;
+    protected double health;
+    protected double power;
 
-	protected abstract boolean attack(MiddleEarthCharacter target);
-	protected abstract String getRace();
-	public void displayInfo() {
-		System.out.println("Name: " + name + "\nHealth: " + health + "\nPower: " + power + "\nRace: " + getRace());
-	}
+    /**
+     * Constructor to initialize a MiddleEarthCharacter with the given attributes.
+     * However, the current implementation incorrectly resets values to 0.
+     * 
+     * @param name   The name of the character.
+     * @param health The health points of the character.
+     * @param power  The attack power of the character.
+     */
+    public MiddleEarthCharacter(String name, double health, double power) {
+        this.name = name;
+        this.health = health;
+        this.power = power;
+    }
+
+    /**
+     * Abstract method that defines how the character attacks another character. The
+     * attack is only successful if the target is not of the same race.
+     * 
+     * @param target The character being attacked.
+     * @return true if the attack is successful, false otherwise.
+     */
+    protected abstract boolean attack(MiddleEarthCharacter target);
+
+    /**
+     * Abstract method to get the race of the character.
+     * 
+     * @return The race of the character as a string.
+     */
+    protected abstract String getRace();
+
+    /**
+     * Displays information about the character, including their name, health,
+     * power, and race.
+     */
+    public void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Health: " + health);
+        System.out.println("Power: " + power);
+        System.out.println("Race: " + getRace());
+    }
 }
