8b296b4843b33be930eb3995af38a8fce6ade862
Gradon Burns <gradyhburns@gmail.com>
2025-03-01 15:32:59 -0600

    Added Dwarf class

---
+++ src/edu/gz/MiddleEarth/Dwarf.java
@@ -0,0 +1,46 @@
+package edu.gz.MiddleEarth;
+
+/**
+ * Represents a Dwarf in Middle-earth.
+ */
+public class Dwarf extends MiddleEarthCharacter {    
+
+    /**
+     * Constructor to initialize a Dwarf character.
+     * 
+     * @param name   The name of the Dwarf.
+     * @param health The Dwarf's health points.
+     * @param power  The Dwarf's attack power.
+     */
+    public Dwarf(String name, double health, double power) {
+        super(name, health, power);
+    }
+    
+    /**
+     * Attacks another character. Dwarfs cannot attack their own kind.
+     * 
+     * @param target The character being attacked.
+     * @return true if the attack was successful, false otherwise.
+     */
+    @Override
+    public boolean attack(MiddleEarthCharacter target) {
+        if (target.getRace().equals(this.getRace())) {
+            return false;
+        }
+
+        // Deal damage while ensuring health doesn't go negative
+        target.health = target.health - power > 0 ? target.health - power : 0;
+
+        return true;
+    }
+
+    /**
+     * Returns the race of the character.
+     * 
+     * @return "Dwarf" as the race of this character.
+     */
+    @Override
+    public String getRace() {
+        return "Dwarf";
+    }
+}
